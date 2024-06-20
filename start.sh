3b56ac44bc59c255d5d1f19e85271bad69819ee2
Nehapj143 <prahacherishedmoments@gmail.com>
2024-06-20 23:45:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-python3 main.py
+
+# Activate the virtual environment
+source /opt/render/project/src/.venv/bin/activate
+
+# Install dependencies listed in requirements.txt
+pip install -r requirements.txt
+
+# Run your Python application
+python /opt/render/project/src/main.py
