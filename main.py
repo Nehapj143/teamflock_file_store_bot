c442fe214007e0024a57e76ec3c4e78f7946b7fc
Nehapj143 <prahacherishedmoments@gmail.com>
2024-06-21 02:30:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import logging
-import os
 import asyncio
-from telegram import Update, InputFile, Bot
-from telegram.constants import ParseMode  # Correct import for ParseMode
-from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, CallbackContext
-from telegram.ext import filters  # Import filters directly from telegram.ext
+from telegram import Bot, Update
+from telegram.constants import ParseMode
+from telegram.ext import Updater, CommandHandler, MessageHandler, ConversationHandler, CallbackContext, Filters
 import uuid
 
 # Enable logging
@@ -16,8 +14,8 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 # Replace with your specific bot token and channel ID
-BOT_TOKEN = "7167327959:AAFJ25AIsO9olQrSzV2OcM0YqY7yUzWekDQ"
-CHANNEL_ID = "-1001329275814"
+BOT_TOKEN = "YOUR_BOT_TOKEN_HERE"
+CHANNEL_ID = "YOUR_CHANNEL_ID_HERE"
 
 # Owners
 OWNERS = [6804487024, 930652019]
@@ -35,21 +33,21 @@ FILE_UPLOAD, FILE_CONFIRMATION = range(2)
 START_THUMBNAIL_URL = "https://i.ibb.co/FbzmyMj/Whats-App-Image-2024-06-20-at-22-00-27-3fc70e42.jpg"
 START_MESSAGE = 'Hi! Send me a file or batch of files and I will store it.'
 
-def start(update: Update, context: CallbackContext) -> None:
+async def start(update: Update, context: CallbackContext) -> None:
     """Send a welcome message with a thumbnail and start the bot"""
     user_id = update.message.from_user.id
     if user_id in OWNERS:
         # Send the thumbnail image
-        context.bot.send_photo(
+        await context.bot.send_photo(
             chat_id=update.message.chat_id,
             photo=START_THUMBNAIL_URL,
             caption=START_MESSAGE,
             parse_mode=ParseMode.HTML  # Using ParseMode.HTML for HTML formatting
         )
     else:
-        update.message.reply_text('You are not authorized to use this bot.')
+        await update.message.reply_text('You are not authorized to use this bot.')
 
-def handle_document(update: Update, context: CallbackContext) -> int:
+async def handle_document(update: Update, context: CallbackContext) -> int:
     """Handle document uploads from users"""
     user_id = update.message.from_user.id
     if user_id in OWNERS:
@@ -60,17 +58,17 @@ def handle_document(update: Update, context: CallbackContext) -> int:
         file = update.message.document
         context.user_data['files'].append(file)
         
-        update.message.reply_text('File received! Send more files or use /done to finish uploading.')
+        await update.message.reply_text('File received! Send more files or use /done to finish uploading.')
         return FILE_UPLOAD
     else:
-        update.message.reply_text('You are not authorized to use this bot.')
+        await update.message.reply_text('You are not authorized to use this bot.')
 
-def done(update: Update, context: CallbackContext) -> None:
+async def done(update: Update, context: CallbackContext) -> None:
     """Finalize file upload and generate a unique link"""
     user_id = update.message.from_user.id
     if user_id in OWNERS:
         if 'files' not in context.user_data or not context.user_data['files']:
-            update.message.reply_text('No files to store.')
+            await update.message.reply_text('No files to store.')
             return
         
         # Generate a unique ID for this batch of files
@@ -79,7 +77,7 @@ def done(update: Update, context: CallbackContext) -> None:
         
         # Send the files to the channel
         for file in context.user_data['files']:
-            context.bot.send_document(
+            await context.bot.send_document(
                 chat_id=CHANNEL_ID, 
                 document=file.file_id,
                 caption=file.file_name
@@ -89,7 +87,7 @@ def done(update: Update, context: CallbackContext) -> None:
         bot_link = f'https://t.me/{context.bot.username}?start={unique_id}'
         render_link = f'{RENDER_URL}/{unique_id}'
         
-        update.message.reply_text(
+        await update.message.reply_text(
             f'Files stored successfully! Share these links to access the files:\n'
             f'Telegram Bot: {bot_link}\n'
             f'Render: {render_link}'
@@ -99,39 +97,37 @@ def done(update: Update, context: CallbackContext) -> None:
         context.user_data.clear()
         return ConversationHandler.END
     else:
-        update.message.reply_text('You are not authorized to use this bot.')
+        await update.message.reply_text('You are not authorized to use this bot.')
 
-def handle_start(update: Update, context: CallbackContext) -> None:
+async def handle_start(update: Update, context: CallbackContext) -> None:
     """Handle the /start command with optional parameters"""
     if context.args:
         unique_id = context.args[0]
         if unique_id in file_store:
             files = file_store[unique_id]
             for file in files:
-                update.message.reply_document(
+                await update.message.reply_document(
                     file.file_id,
                     caption=file.file_name
                 )
         else:
-            update.message.reply_text('Invalid or expired link.')
+            await update.message.reply_text('Invalid or expired link.')
     else:
-        update.message.reply_text('Hi! Send me a file or batch of files and I will store it.')
+        await update.message.reply_text('Hi! Send me a file or batch of files and I will store it.')
 
 async def main() -> None:
     """Start the bot"""
     bot = Bot(token=BOT_TOKEN)
-    update_queue = asyncio.Queue()  # Create an asyncio Queue for updates
-
-    updater = Updater(bot=bot, update_queue=update_queue)
+    updater = Updater(bot=bot, use_context=True)
     dispatcher = updater.dispatcher
 
     conv_handler = ConversationHandler(
-        entry_points=[MessageHandler(filters.Filters.document, handle_document)],  # Using filters.Filters
+        entry_points=[MessageHandler(Filters.document, handle_document)],
         states={
-            FILE_UPLOAD: [MessageHandler(filters.Filters.document, handle_document)],  # Using filters.Filters
+            FILE_UPLOAD: [MessageHandler(Filters.document, handle_document)],
             FILE_CONFIRMATION: [CommandHandler('done', done)],
         },
-        fallbacks=[CommandHandler('start', start)],
+        fallbacks=[CommandHandler('start', handle_start, pass_args=True)],
     )
 
     dispatcher.add_handler(conv_handler)
